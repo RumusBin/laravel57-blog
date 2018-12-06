abd99787c475f794b8f5e3a892567bc1c4367249
Ruslan Komlikov <komlikov.r@gmail.com>
2018-12-06 16:53:46 +0200

    add spatie

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,36 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 </head>
 <body>
+<!-- Right Side Of Navbar -->
+    <ul class="navbar-nav ml-auto">
+        <!-- Authentication Links -->
+        @guest
+        <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+        <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
+        @else
+            <li><a class="nav-link" href="{{ route('users.index') }}">Manage Users</a></li>
+            <li><a class="nav-link" href="{{ route('roles.index') }}">Manage Role</a></li>
+            <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    {{ Auth::user()->name }} <span class="caret"></span>
+                </a>
+
+
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                       onclick="event.preventDefault();
+                                                         document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </div>
+            </li>
+            @endguest
+    </ul>
     @yield('content')
     @include('partials._footer')
     @yield('scripts')
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,15 @@ Route::get('/advertising', 'PagesController@advertising')->name('advertising');
 Route::get('/about', 'PagesController@about')->name('about');
 Route::get('/contacts', 'PagesController@contacts')->name('contacts');
 
-Route::resource('categories', 'CategoryController');
-Route::resource('posts', 'PostController');
-Route::resource('info', 'InfoController');
-Route::resource('price-blocks', 'PriceBlockController');
+Route::group(['middleware' => ['auth']], function() {
+    Route::resource('roles','RoleController');
+    Route::resource('users','UserController');
+    Route::resource('categories', 'CategoryController');
+    Route::resource('posts', 'PostController');
+    Route::resource('info', 'InfoController');
+    Route::resource('price-blocks', 'PriceBlockController');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
