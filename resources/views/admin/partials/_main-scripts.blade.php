2165f2f1bae90eb4c010b7e5b8855758a9eacfe7
Ruslam Komlikov <komlikov.r@gmail.com>
2019-03-18 10:35:37 +0200

    Fix js includes

--- resources/views/admin/partials/_main-scripts.blade.php
+++ resources/views/admin/partials/_main-scripts.blade.php
@@ -5,15 +5,15 @@
 <!-- FastClick -->
 <script src="{{asset('js/admin/fastclick/fastclick.js')}}"></script>
 <!-- AdminLTE App -->
-<script src="{{'js/admin/adminlte.min.js'}}"></script>
+<script src="{{asset('js/admin/adminlte.min.js')}}"></script>
 <!-- Sparkline -->
-<script src="bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+{{--<script src="bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>--}}
 <script src="{{asset('ja/admin/jquery-sparkline/jquery.sparkline.min.js')}}"></script>
 <!-- jvectormap  -->
 <script src="{{asset('js/admin/jvectormap/jquery-jvectormap-1.2.2.min.js')}}"></script>
 <script src="{{asset('js/admin/jvectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
 <!-- SlimScroll -->
-<script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+{{--<script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>--}}
 <script src="{{asset('js/admin/jquery-slimscroll/jquery.slimscroll.min.js')}}"></script>
 <!-- ChartJS -->
 <script src="{{asset('js/admin/chart_js/Chart.js')}}"></script>
